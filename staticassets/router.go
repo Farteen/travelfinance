6b8757c442df79901bfa7d9d12c9aee4435c1336
farteen <xujavacn@gmail.com>
2019-03-18 06:48:25 +0800

    add static file router

--- staticassets/router.go
+++ staticassets/router.go
@@ -1,7 +1,12 @@
 package staticassets
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func RegisterRouter(engine *gin.Engine) {
-	engine.StaticFile("/static", "./")
+	//engine.StaticFile("/static", "/Users/glassesd/Desktop/static_assets")
+	engine.Static("/static", "./static_assets")
+	engine.StaticFS("/sfs", http.Dir("static_assets_fs"))
 }
