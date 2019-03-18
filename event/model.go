7c6e32d70f7534e7cd3dd88b0a078b845511b737
Xu Jiawei <jiawei.xu@centrixlink.com>
2019-03-18 15:20:08 +0800

    edit event item logic

--- event/model.go
+++ event/model.go
@@ -4,12 +4,12 @@ import "github.com/mongodb/mongo-go-driver/bson/primitive"
 
 type EventItem struct {
 	EventID primitive.ObjectID `bson:"_id,omitempty" json:"eventId"`
-	GroupID primitive.ObjectID `bson:"group_id" json:"group_id"`
 	CreatorID primitive.ObjectID	`bson:"creatorId" json:"creatorId"`
+	Users []primitive.ObjectID `bson:"users" json:"users"`
 	Name string	`bson:"name" json:"name"`
+	EventType int `bson:"event_type" json:"event_type"`
+	CreateTimestamp int32 `bson:"create_timestamp" json:"create_timestamp"`
 	Description string	`bson:"desc" json:"desc"`
 	Images []string	`bson:"images" json:"images"`
 }
-
-
 
--- usergroup/model.go
+++ usergroup/model.go
@@ -3,7 +3,7 @@ package usergroup
 import "github.com/mongodb/mongo-go-driver/bson/primitive"
 
 type UserGroupRequest struct {
-	GroupID primitive.ObjectID `json:"group_id" bson:"group_id,omitempty"`
+	GroupID primitive.ObjectID `json:"group_id" bson:"_id,omitempty"`
 	GroupName string `json:"group_name" bson:"group_name"`
 	MaintainerId primitive.ObjectID `json:"maintainer_id" bson:"maintainer_id"`
 	Users []primitive.ObjectID `json:"users" bson:"users"`
